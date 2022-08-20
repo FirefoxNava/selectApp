85646f989207a495112261606f9842eca2b10b3a
Michael Navarrete <mnavarr09665@universidadean.edu.co>
2022-08-19 19:44:15 -0500

    Conexión Backend - FrontEnd
    Se ha corregido error CORS en Backend

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="container">
     <div class="search">
-      <input type="text" placeholder="Select an item" 
-        v-model="inputData" 
+      <input
+        type="text"
+        placeholder="Select an item"
+        v-model="inputData"
         v-on:focusin="showResults()"
         v-on:focusout="disableResults()"
-        v-on:input="filterFruit(inputData)">
+        v-on:input="filterFruit(inputData)"
+      />
       <div class="icon">
         <arrowIcon></arrowIcon>
       </div>
@@ -20,66 +23,75 @@
 </template>
 
 <script>
+//Librerias
+import axios from "axios";
 
 //Iconos
-import arrowIcon from './components/icons/arrowIcon.vue';
+import arrowIcon from "./components/icons/arrowIcon.vue";
 
 export default {
-
   components: {
     //Iconos
-    arrowIcon
+    arrowIcon,
   },
   data() {
     return {
-      fruitData: ['Broccoli', 'Lettuce', 'Peppers', 'Root vegetables', 'Cucumber', 'Parsley'],
+      fruitData: [],
       inputData: null,
       activeStatus: false,
-      filteredData : [],
-      errorStatus: false
-    }
+      filteredData: [],
+      errorStatus: false,
+    };
   },
   methods: {
-    getFruitData() {
-      return false
+    async getFruitData() {
+      await axios
+        .get(import.meta.env.VITE_API_URL + "/api/fruits")
+        .then((response) => {
+          if (response.status === 200) {
+            this.fruitData = response.data.fruits;
+          }
+        });
     },
     filterFruit(inputData) {
-      let emptyArray = []
+      let emptyArray = [];
 
       //Filtrar Datos
 
       if (inputData) {
         //Generar un filtro en base a las letras iniciales
         emptyArray = this.fruitData.filter((item) => {
-          return item.toLocaleLowerCase().startsWith(inputData.toLocaleLowerCase())
-        })
-        console.log(emptyArray.length)
-        this.filteredData = emptyArray
+          return item
+            .toLocaleLowerCase()
+            .startsWith(inputData.toLocaleLowerCase());
+        });
+        console.log(emptyArray.length);
+        this.filteredData = emptyArray;
 
-        if(emptyArray.length===0){
-          this.errorStatus = true
+        if (emptyArray.length === 0) {
+          this.errorStatus = true;
         } else {
-          this.errorStatus = false
+          this.errorStatus = false;
         }
       } else {
-        this.errorStatus = false
-        this.filteredData = this.fruitData
-      } 
+        this.errorStatus = false;
+        this.filteredData = this.fruitData;
+      }
     },
     showResults() {
-      this.activeStatus = true
-      this.filterFruit(this.inputData)
+      this.activeStatus = true;
+      this.filterFruit(this.inputData);
     },
     disableResults() {
-      this.activeStatus = false
-    }
-  }
-}
-
+      this.activeStatus = false;
+    },
+  },
+  mounted() {
+    this.getFruitData();
+  },
+};
 </script>
 
-<style >
+<style>
 @import "./assets/main.css";
 </style>
-
-
